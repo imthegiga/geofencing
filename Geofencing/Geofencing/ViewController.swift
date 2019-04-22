c11783eeb6d98cd3a4a81eab371eac629f4ddf9e
Abhishek Salokhe (HLB) <AbhishekSalokhe@hlbb.hongleong.com.my>
2019-04-22 19:13:48 +0800

    Initial Commit

---
+++ Geofencing/Geofencing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Geofencing
+//
+//  Created by Abhishek Salokhe on 22/04/2019.
+//  Copyright © 2019 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
